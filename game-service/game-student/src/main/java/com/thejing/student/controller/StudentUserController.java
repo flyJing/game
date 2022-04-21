a502b55c313f2cafe3b826af8692f2ddb84fd405
flyJing <tj15676818429@qq.com>
2022-04-21 13:44:38 +0800

    高校赛事报销平台

--- game-service/game-student/src/main/java/com/thejing/student/controller/StudentUserController.java
+++ game-service/game-student/src/main/java/com/thejing/student/controller/StudentUserController.java
@@ -3,12 +3,12 @@ package com.thejing.student.controller;
 
 import com.thejing.model.common.dtos.ResponseResult;
 import com.thejing.model.common.dtos.StudentLoginDto;
+import com.thejing.model.common.enums.AppHttpCodeEnum;
+import com.thejing.model.common.pojos.GameAdmin;
+import com.thejing.model.common.pojos.GameStudent;
 import com.thejing.student.service.StudentUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,6 +23,18 @@ public class StudentUserController {
     @PostMapping("/login_auth")
     public ResponseResult login(@RequestBody StudentLoginDto dto, HttpServletRequest request){
         return studentUserService.login(dto,request);
+    }
+    @PostMapping("/findUsername")
+    public ResponseResult findUsername(HttpServletRequest request){
+        GameStudent user = (GameStudent) request.getSession().getAttribute("student");
+        return ResponseResult.okResult(user.getUsername());
+    }
+
+    @GetMapping("/logOut")
+    public ResponseResult logOut(HttpServletRequest request){
+        //销毁回话
+        request.getSession().invalidate();
+        return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
     }
 
 }
